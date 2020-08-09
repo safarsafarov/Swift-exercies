72febefd46217ab3d386ed4fba6cc0f3eea6bfa4
safarsafarov <safarov.safar@hotmail.com>
2020-08-09 10:10:18 +0800

    exercises

--- exercies.playground/Contents.swift
+++ exercies.playground/Contents.swift
@@ -1,54 +1,80 @@
 import UIKit
 
-class Person {
-    var name: String
-    var secondName: String
-    var age: Int
-    var location: String
-    var job: Bool
-    init(name: String, secondName: String, age: Int, location: String, job: Bool) {
-        self.name = name
-        self.secondName = secondName
-        self.age = age
-        self.location = location
-        self.job = job
-    }
+// classes in swift
+//class Person {
+//    var name: String
+//    var secondName: String
+//    var age: Int
+//    var location: String
+//    var job: Bool
+//    init(name: String, secondName: String, age: Int, location: String, job: Bool) {
+//        self.name = name
+//        self.secondName = secondName
+//        self.age = age
+//        self.location = location
+//        self.job = job
+//    }
+//}
+//
+//class citizens {
+//    var citizenName: String
+//    var citizenSurname:String
+//    var citizenId: Int
+//    var citizenJob: Bool
+//    var citizenIsMarried: Bool
+//    var citizenHasChilderen: Bool
+//    init(citizenName: String, citizenSurname: String, citizenId: Int, citizenJob: Bool, citizenIsMarried: Bool, citizenHasChilderen: Bool) {
+//        self.citizenName = citizenName
+//        self.citizenSurname = citizenSurname
+//        self.citizenId = citizenId
+//        self.citizenJob = citizenJob
+//        self.citizenIsMarried = citizenIsMarried
+//        self.citizenHasChilderen = citizenHasChilderen
+//    }
+//}
+//
+
+
+//A class can inherit methods, properties, and other characteristics from another class.
+//class Vehicle {
+//    var currentSpeed = 0.0
+//    var description: String {
+//        return "traveling at \(currentSpeed) miles per hour"
+//    }
+//    func makeNoise() {
+//        // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+//    }
+//}
+
+//print("Vehicle: \(someVehicle.description)")
+// Vehicle: traveling at 0.0 miles per hour
+
+//Subclassing
+//class SomeSubclass: SomeSuperclass {
+    // subclass definition goes here
 }
+//class Bicycle: Vehicle {
+//    var hasBasket = false
+//}
 
-let info = Person(name: "Safar", secondName: "Safarov", age: 18, location: "China", job: true)
-print(info)
-
-struct Human {
-  var name: String
-    var secondName: String
-    var age: Int
-    var location: String
-    var job: Bool
-    init(name: String, secondName: String, age: Int, location: String, job: Bool) {
-        self.name = name
-        self.secondName = secondName
-        self.age = age
-        self.location = location
-        self.job = job
-    }
-}
-
-let info2 = Human(name: "Safar", secondName: "Safarov", age: 19, location: "China", job: true)
-print(info2)
-
-class citizens {
-    var citizenName: String
-    var citizenSurname:String
-    var citizenId: Int
-    var citizenJob: Bool
-    var citizenIsMarried: Bool
-    var citizenHasChilderen: Bool
-    init(citizenName: String, citizenSurname: String, citizenId: Int, citizenJob: Bool, citizenIsMarried: Bool, citizenHasChilderen: Bool) {
-        self.citizenName = citizenName
-        self.citizenSurname = citizenSurname
-        self.citizenId = citizenId
-        self.citizenJob = citizenJob
-        self.citizenIsMarried = citizenIsMarried
-        self.citizenHasChilderen = citizenHasChilderen
-    }
-}
+//let info = Person(name: "Safar", secondName: "Safarov", age: 18, location: "China", job: true)
+//print(info)
+//
+//struct Human {
+//  var name: String
+//    var secondName: String
+//    var age: Int
+//    var location: String
+//    var job: Bool
+//    init(name: String, secondName: String, age: Int, location: String, job: Bool) {
+//        self.name = name
+//        self.secondName = secondName
+//        self.age = age
+//        self.location = location
+//        self.job = job
+//    }
+//}
+//
+//let info2 = Human(name: "Safar", secondName: "Safarov", age: 19, location: "China", job: true)
+//print(info2)
+//
