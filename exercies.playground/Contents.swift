02bd2d0382891eb00d514544563ba62ffdc552c6
safarsafarov <safarov.safar@hotmail.com>
2020-08-16 19:41:45 +0800

    ex

--- exercies.playground/Contents.swift
+++ exercies.playground/Contents.swift
@@ -95,95 +95,135 @@ import UIKit
 //Specifically, the result of an optional chaining call is of the same type as the expected return value, but wrapped in an optional. A property that normally returns an Int will return an Int? when accessed through optional chaining.
 //
 //The next several code snippets demonstrate how optional chaining differs from forced unwrapping and enables you to check for success.
-class Person {
-    var residence: Residence?
-}
+//class Person {
+//    var residence: Residence?
+//}
+//
+//class Residence {
+//    var rooms = [Room]()
+//    var numberOfRooms: Int {
+//        return rooms.count
+//    }
+//    subscript(i: Int) -> Room {
+//        get {
+//            return rooms[i]
+//        }
+//        set {
+//            rooms[i] = newValue
+//        }
+//    }
+//    func printNumberOfRooms() {
+//        print("The number of rooms is \(numberOfRooms)")
+//    }
+//    var address: Address?
+//}
+//
+//class Room {
+//    let name: String
+//    init(name: String) { self.name = name }
+//}
+//
+//class Address {
+//    var buildingName: String?
+//    var buildingNumber: String?
+//    var street: String?
+//    func buildingIndentifier() -> String? {
+//        if let buildingNumber = buildingNumber, let street = street {
+//            return "\(buildingNumber) \(street)"
+//        } else if buildingName != nil {
+//            return buildingName
+//        } else {
+//            return nil
+//        }
+//    }
+//}
+//
+//let john = Person()
+//
+//if let roomCount = john.residence?.numberOfRooms {
+//    print("John's residence has \(roomCount) rooms(s).")
+//} else {
+//    print("Unable to retrieve the number of rooms.")
+//}
+//// Print "Unable to retrieve the number of rooms.
+//
+//let someAddress = Address()
+//someAddress.buildingNumber = "29"
+//someAddress.street = "Acacia Road"
+//john.residence?.address = someAddress
+//
+//func createAddress() -> Address {
+//    print("Function was called.")
+//    let someAddress = Address()
+//    someAddress.buildingNumber = "29"
+//    someAddress.street = "Acacia Road"
+//
+//    return someAddress
+//}
+//
+//john.residence?.address = createAddress()
+//if (john.residence?.address = someAddress) != nil {
+//    print("It was possible to set the address.")
+//} else {
+//    print("It was not possible to set the address.")
+//}
+//
+//if let firstRoomName = john.residence?[0].name {
+//    print("The first room name is \(firstRoomName).")
+//} else {
+//    print("Unable to retrieve the first room name.")
+//}
+//// Prints "Unable to retrieve the first room name."
+//john.residence?[0] = Room(name: "Bathroom")
+//
+//let johnHouse = Residence()
+//johnHouse.rooms.append(Room(name: "Living Room"))
+//johnHouse.rooms.append(Room(name: "Kitch"))
+//john.residence = johnHouse
+//
+//if let firstRoomName = john.residence?[0].name {
+//    print("The first room name is \(firstRoomName).")
+//} else {
+//    print("Unable to retrieve the first room name.")
+//}
+//// Prints "The first room name is Living Room."
+//
+//var testScores = ["Dave": [86, 82, 84], "Bev": [79, 94,81]]
+//testScores["Dave"]?[0] = 91
+//testScores["Bev"]?[0] += 1
+//testScores["Brian"]?[0] = 72
+//// the "Dave" array is now [91, 82, 84] and the "Bev" array is now [80, 94, 81]
+//
+//if let johnStreet = john.residence?.address?.street {
+//    print("John's street name is \(johnStreet).")
+//} else {
+//    print("Unale to retrieve the address.")
+//}
+//// Prints "Unable to retrieve the address."
+//
+//let johnAddress = Address()
+//johnsAddress.buildingName = "The Larches"
+//johnsAddress.street = "Laurel Street"
+//johnsAddress.residence?.address = johnsAddress
+//
+//if let johnStreet = john.residence?.address?.street {
+//    print("John's street name is \(johnsStreet).")
+//} else {
+//    print("Unable to retrieve the address.")
+//}
+//// Prints "John's street name is Laurel Street
+//
+//if let buildingIdentifier = john.residence?.address?.buildingIdentifier() {
+//    print("John's bullding indetifier is \(buildingIdentiier).")
+//}
+//// Prints "John's building indetifier is The Larches."
+//
+//if let beginsWithThe =
+//    john.residence?.address?.buildingIdentifier()?.hasPrefix("The") {
+//if beginsWithThe {
+//    print("John's building indetifier begins with \"The\".")
+//} else {
+//    print("John's building indetifier does not begin with \"The\".")
+//// Prints "John's building identifier begins with "The"."
 
-class Residence {
-    var rooms = [Room]()
-    var numberOfRooms: Int {
-        return rooms.count
-    }
-    subscript(i: Int) -> Room {
-        get {
-            return rooms[i]
-        }
-        set {
-            rooms[i] = newValue
-        }
-    }
-    func printNumberOfRooms() {
-        print("The number of rooms is \(numberOfRooms)")
-    }
-    var address: Address?
-}
-
-class Room {
-    let name: String
-    init(name: String) { self.name = name }
-}
-
-class Address {
-    var buildingName: String?
-    var buildingNumber: String?
-    var street: String?
-    func buildingIndentifier() -> String? {
-        if let buildingNumber = buildingNumber, let street = street {
-            return "\(buildingNumber) \(street)"
-        } else if buildingName != nil {
-            return buildingName
-        } else {
-            return nil
-        }
-    }
-}
-
-let john = Person()
-
-if let roomCount = john.residence?.numberOfRooms {
-    print("John's residence has \(roomCount) rooms(s).")
-} else {
-    print("Unable to retrieve the number of rooms.")
-}
-// Print "Unable to retrieve the number of rooms.
-
-let someAddress = Address()
-someAddress.buildingNumber = "29"
-someAddress.street = "Acacia Road"
-john.residence?.address = someAddress
-
-func createAddress() -> Address {
-    print("Function was called.")
-    let someAddress = Address()
-    someAddress.buildingNumber = "29"
-    someAddress.street = "Acacia Road"
-    
-    return someAddress
-}
-
-john.residence?.address = createAddress()
-if (john.residence?.address = someAddress) != nil {
-    print("It was possible to set the address.")
-} else {
-    print("It was not possible to set the address.")
-}
-
-if let firstRoomName = john.residence?[0].name {
-    print("The first room name is \(firstRoomName).")
-} else {
-    print("Unable to retrieve the first room name.")
-}
-// Prints "Unable to retrieve the first room name."
-john.residence?[0] = Room(name: "Bathroom")
-
-let johnHouse = Residence()
-johnHouse.rooms.append(Room(name: "Living Room"))
-johnHouse.rooms.append(Room(name: "Kitch"))
-john.residence = johnHouse
-
-if let firstRoomName = john.residence?[0].name {
-    print("The first room name is \(firstRoomName).")
-} else {
-    print("Unable to retrieve the first room name.")
-}
-// Prints "The first room name is Living Room."
+// That was the end of Optionals
