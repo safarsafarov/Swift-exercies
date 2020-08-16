fdb59ac53c91fc62704995ba94a255b4baad0042
safarsafarov <safarov.safar@hotmail.com>
2020-08-16 13:33:24 +0800

    1

--- exercies.playground/Contents.swift
+++ exercies.playground/Contents.swift
@@ -161,3 +161,8 @@ func createAddress() -> Address {
     return someAddress
 }
 john.residence?.address = createAddress()
+if (john.residence?.address = someAddress) != nil {
+    print("It was possible to set the address.")
+} else {
+    print("It was not possible to set the address.")
+}
